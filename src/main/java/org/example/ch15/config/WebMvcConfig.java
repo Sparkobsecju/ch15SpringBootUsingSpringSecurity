19a79e2e0f0683fdafa0e808da896074f50df7ec
ubuntu <ryan1825114@gmail.com>
2024-07-11 23:35:15 -0700

    define a class that implements WebMvcConfigurer as a ViewController, so that we don't really have to create a controller to map request that Spring Boot Application receive

---
+++ src/main/java/org/example/ch15/config/WebMvcConfig.java
@@ -0,0 +1,15 @@
+package org.example.ch15.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/home").setViewName("home");
+        registry.addViewController("/resource").setViewName("resource");
+        registry.addViewController("/login").setViewName("login");
+    }
+}
